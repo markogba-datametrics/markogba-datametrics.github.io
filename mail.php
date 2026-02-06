7b5a9aeef95d90fdbd8eade83e7383502d04a0ca
Mark Ogba <Markogba5@gmail.com>
2026-02-06 12:36:32 +0100

    Update mail.php

--- mail.php
+++ mail.php
@@ -4,11 +4,11 @@
 $name = $_POST['name'];
 $email= $_POST['email'];
 $message= $_POST['message'];
-$to = "nelsongodwill95@outlook.com";
+$to = "Markogba5@gmail.com";
 $subject = "Mail From Portfolio Website";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
-$headers = "From: noreply@nelson-analytics.com" . "\r\n" .
-"CC: nelsongodwill95@gmail.com";
+$headers = "From: noreply@markogba-datametrics.com" . "\r\n" .
+"CC: Markogba5@gmail.com";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
 }
